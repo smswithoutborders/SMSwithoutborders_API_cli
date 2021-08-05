8f89ed52581db60a3b14c8fed59a82ed07ab053a
Promise Fru <promisefru1@gmail.com>
2021-08-05 12:11:30 +0100

    updates

--- index.js
+++ index.js
@@ -10,12 +10,5 @@ program.version('0.0.1', '-v, --vers', 'output the current version');
 program
     .command('create', "Creates a new Provider/Platform with empty template")
 
-program.addHelpText('after', `
-Examples:
-  $ swob -v
-  $ swob -h
-  $ swob create [options]
-  $ swob help [command]`);
-
 program.showHelpAfterError();
 program.parse();
--- swob-create.js
+++ swob-create.js
@@ -7,7 +7,7 @@ const program = new Command();
 
 program
     .requiredOption("-pr, --provider <name>", "Provider's name")
-    .option("-pl, --platform <name>", "Platform's name")
+    .requiredOption("-pl, --platform <name>", "Platform's name")
     .action((options) => {
         let provider = options.provider;
         let platform = options.platform;
@@ -18,7 +18,7 @@ program
 program.addHelpText('after', `
 Examples:
   $ swob create -pr <name> -pl <name>
-  $ swob create -h`
-  );
+`);
+
 program.showHelpAfterError();
 program.parse();
