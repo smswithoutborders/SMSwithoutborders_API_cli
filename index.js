f27629231fc2cd0a41d2e49db802a142b14cbbb2
Promise Fru <promisefru1@gmail.com>
2021-08-09 21:04:19 +0100

    working on install command

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ program.version('0.0.1', '-v, --vers', 'output the current version');
 program
     .command('create', "Create a new Provider/Platform with empty template")
     .command('delete', "Delete Provider/Platform")
+    .command('install', "Download Provider/Platform from github")
 
 program.showHelpAfterError();
 program.parse();
